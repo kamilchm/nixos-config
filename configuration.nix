28b3b41366b37abe6014b161abadb0cf408201c3
Kamil Chmielewski <kamil.chm@gmail.com>
2015-07-18 13:22:39 +0200

    NixOS theme for slim got merged as a default

--- configuration.nix
+++ configuration.nix
@@ -117,13 +117,7 @@
         xscreensaver -no-splash &
       '';
 
-      displayManager.slim = {
-        enable = true;
-        theme = pkgs.fetchurl {
-          url    = "https://github.com/jagajaga/nixos-slim-theme/archive/Final.tar.gz";
-          sha256 = "4cab5987a7f1ad3cc463780d9f1ee3fbf43603105e6a6e538e4c2147bde3ee6b";
-        };
-      };
+      displayManager.slim.enable = true;
     };
   };
 
