9476cf179e125b462e214f16f24c840e1f4fec09
Kamil Chmielewski <kamil.chm@gmail.com>
2018-02-08 10:04:40 +0100

    linux: 4.12 -> 4.14

--- configuration.nix
+++ configuration.nix
@@ -24,7 +24,7 @@
   boot = {
     vesa = false;
 
-    kernelPackages = pkgs.linuxPackages_4_12;
+    kernelPackages = pkgs.linuxPackages_4_14;
 
     initrd = {
       kernelModules = [ "xhci_hcd" "ehci_pci" "ahci" "usb_storage" "aesni-intel" "i915" ];
